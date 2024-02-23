74e0bb12755e1d21422a64df864514da1db544ea
Shreeraam G <shreeram0312@gmail.com>
2024-02-23 21:25:33 +0530

    Solved: Game of Two Stacks (using Recursion)

--- src/problemSolving/stacks/GameOfTwoStacks.java
+++ src/problemSolving/stacks/GameOfTwoStacks.java
@@ -1,40 +1,25 @@
 // https://www.hackerrank.com/challenges/game-of-two-stacks/problem
 package problemSolving.stacks;
-import java.util.Stack;
+import java.util.Arrays;
 
 public class GameOfTwoStacks {
     public static void main(String[] args) {
-        Stack<Integer> stackOne = new Stack<>();
-        Stack<Integer> stackTwo = new Stack<>();
+        int[] s1 = {4, 2, 4, 6, 1};
+        int[] s2 = {2, 1, 8, 5};
 
-        // Inserting elements to the stack
-        stackOne.push(1);
-        stackOne.push(6);
-        stackOne.push(4);
-        stackOne.push(2);
-        stackOne.push(4);
+        System.out.println(getMaximumMoves(10, s1, s2, 0, 0) - 1);
+    }
 
-        stackTwo.push(5);
-        stackTwo.push(8);
-        stackTwo.push(1);
-        stackTwo.push(2);
+    static int getMaximumMoves(int limit, int[] s1, int[] s2, int sumSoFar, int count) {
+        if(sumSoFar > limit)
+            return count;
 
-        int maxValue = 10;
-        int sum = 0;
-        int moves = 0;
+        if(s1.length == 0 || s2.length == 0)
+            return count;
 
-        while(true) {
-            if(stackOne.peek() <= stackTwo.peek())
-                sum += stackOne.pop();
-            else
-                sum += stackTwo.pop();
+        int ans1 = getMaximumMoves(limit, Arrays.copyOfRange(s1, 1, s1.length), s2, sumSoFar + s1[0], count+1);
+        int ans2 = getMaximumMoves(limit, s1, Arrays.copyOfRange(s2, 1, s2.length), sumSoFar + s2[0], count+1);
 
-            if(sum < maxValue) {
-                moves++;
-            } else {
-                System.out.println(moves);
-                break;
-            }
-        }
+        return Math.max(ans1, ans2);
     }
 }
